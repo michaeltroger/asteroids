eba916cdd22343225cf6419e96ff7a5161857557
s1310238069@students.fh-hagenberg.at <michaeltroger@users.noreply.github.com>
2016-09-18 23:39:06 +0200

    added (c) added pic

--- core/src/at/fhoe/mtd/igp/World.java
+++ core/src/at/fhoe/mtd/igp/World.java
@@ -1,18 +1,19 @@
 /**
-This file is part of Asteroid Clone by Michael Troger.
+Copyright 2016 Michael Troger <https://michaeltroger.com>
+This file is part of Asteroids Clone with LibGDX.
 
-Asteroid Clone by Michael Troger is free software: you can redistribute it and/or modify
+Asteroids Clone with LibGDX is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Asteroid Clone by Michael Troger is distributed in the hope that it will be useful,
+Asteroids Clone with LibGDX is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Asteroid Clone by Michael Troger.  If not, see <http://www.gnu.org/licenses/>.
+along with Asteroids Clone with LibGDX.  If not, see <http://www.gnu.org/licenses/>.
 */
 package at.fhoe.mtd.igp;
 
